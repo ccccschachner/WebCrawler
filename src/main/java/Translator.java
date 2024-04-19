e1eb7d0ec83ff30c973784f708d9a07abcad5d9a
laulaobm <131969052+laulaobm@users.noreply.github.com>
2024-04-19 18:12:17 +0200

    added almost empty class Translator

---
+++ src/main/java/Translator.java
@@ -0,0 +1,8 @@
+public class Translator {
+    String targetLanguage=Main.getTargetLanguage();
+    String sourceLanguage;
+
+    public String translate(String heading){
+        return "";
+    }
+}
